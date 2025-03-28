03519f2a01d2fea19241fb9e94b84e247da5c412
Karlis Paulus <pauluskarlis@gmail.com>
2025-03-28 23:01:02 +0200

    Initial commit

---
+++ src/test/java/com/example/FlightPlanner/FlightPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FlightPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
